7ac3aa97b6f9fbd9045563340161b9fcd7ccbd3c
Snipcola <snipcola@gmail.com>
2022-11-02 18:55:43 +0000

    rollback

--- components/dashboard/container.jsx
+++ components/dashboard/container.jsx
@@ -1,10 +1,20 @@
 import styles from './container.module.sass';
+import axios from 'axios';
 import { Component, useEffect } from 'react';
+import Links from '../../lib/links';
+import Router from 'next/router';
 
 export default class extends Component {
     state = {
-        loaded: false
+        loaded: false,
+        visible: false
     };
+
+    componentDidMount = () => axios.get('/api/user')
+        .then((r) => r?.data?.data?.user
+            ? this.setState({ visible: true })
+            : Router.push(Links.login))
+        .catch(() => Router.push(Links.login));
 
     render = () => {
         const loading = !this.state.loaded ? (!this.state.loaded ?? this.props.loading) : (this.props.loading ?? !this.state.loaded);
@@ -18,11 +28,17 @@ export default class extends Component {
             <div>
                 <style global jsx>{`html { background-color: #242532 !important; }`}</style>
                 {loading && <div className={styles.loading}><div className={styles.bar} /></div>}
-                <div className={this.props.container ?? styles.container}>
-                    <div className={this.props.content ?? styles.content}>
-                        {this.props.children}
+                {this.state.visible ? (
+                    <div className={this.props.container ?? styles.container}>
+                        <div className={this.props.content ?? styles.content}>
+                            {this.props.children}
+                        </div>
                     </div>
-                </div>
+                ) : (
+                    <div className={styles.container}>
+                        <p className={styles.loadingText}>Authenticating...</p>
+                    </div>
+                )}
             </div>
         );
     };
