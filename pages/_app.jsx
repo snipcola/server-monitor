b0a5390a3e34511bce53cb6cf6f99c4669fd6bee
Snipcola <snipcola@gmail.com>
2022-12-05 22:14:06 +0000

    fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,16 @@
 import '../styles/default.sass';
 import Head from 'next/head';
-import { useEffect, Component } from 'react';
-import TagManager from 'react-gtm-module';
+import { Component } from 'react';
 
 export default class App extends Component {
-    render = ({ Component, pageProps }) => {
-        useEffect(() => TagManager.initialize({ gtmId: 'GTM-5HR709SKZ6' }), []);
+    render = ({ Component, pageProps }) => (
+        <div>
+            <Head>
+                <meta name='viewport' content='width=device-width,initial-scale=1' />
+                <title>Server Monitor - The best monitoring solution</title>
+            </Head>
 
-        return (
-            <div>
-                <Head>
-                    <meta name='viewport' content='width=device-width,initial-scale=1' />
-                    <title>Server Monitor - The best monitoring solution</title>
-                </Head>
-    
-                <Component {...pageProps} />
-            </div>
-        );
-    };
+            <Component {...pageProps} />
+        </div>
+    );
 };
